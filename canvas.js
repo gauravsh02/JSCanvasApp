d091cd8a424a2e8ddc59de7926385e8f2bca9327
gauravsh_ed <gauravsh@edmingle.com>
2020-03-29 01:05:28 +0530

    moved to shapes object

--- canvas.js
+++ canvas.js
@@ -3,82 +3,107 @@ function Canvas(canvas){
   this.canvas = canvas;
   this.height = canvas.height;
   this.width = canvas.width;
-  this.triangleArr = [];
   this.shapesArr = [];
-  this.isOnTriangle = false;
+  this.messageArr = [];
+  this.pointsArray = [];
+  this.isOnPolygon = false;
+  this.pointsLimit = 0;
   this.imx;
   this.imy;
   this.fmx;
   this.fmy;
   var self = this;
   this.ctx = canvas.getContext('2d');
-  //mousedown event listener
+  // mousedown event listener
   canvas.addEventListener('mousedown', function(event){
+    console.log('mousedown event called');
     self.imx = event.clientX;
     self.imy = event.clientY;
-    if(self.triangleArr.length){
+    if(self.shapesArr.length){
       let i=0;
-      self.triangleArr.forEach(function(triangle){
-        if(triangle.checkPos(self.imx, self.imy)){
-          self.isOnTriangle = true;
+      self.shapesArr.forEach(function(triangle){
+        if(triangle.checkPos(self.ctx, self.imx, self.imy)){
+          self.isOnPolygon = true;
           self.selected = triangle;
           self.selectedPos = i;
         }
         i++;
       });
     } else {
-      self.isOnTriangle = false;
+      self.isOnPolygon = false;
     }
     event.preventDefault();
   });
   //mouseup event listener
   canvas.addEventListener('mouseup', function(event){
+    console.log('mouseup event called');
     self.fmx = event.clientX;
     self.fmy = event.clientY;
-    if(self.isOnTriangle){
-      let tri = self.triangleArr[self.selectedPos];
-      self.triangleArr.splice(self.selectedPos, 1);
+    if(self.isOnPolygon){
+      let tri = self.shapesArr[self.selectedPos];
+      self.shapesArr.splice(self.selectedPos, 1);
       let xdiff = self.fmx-self.imx;
       let ydiff = self.fmy-self.imy;
-      tri.x1+=xdiff;
-      tri.y1+=ydiff;
-      tri.x2+=xdiff;
-      tri.y2+=ydiff;
-      tri.x3+=xdiff;
-      tri.y3+=ydiff;
-      self.triangleArr.push(tri);
-      self.isOnTriangle=false;
+      tri.sides.forEach(function(side){
+        side.x = side.x + (xdiff ? xdiff : -xdiff);
+        side.y = side.y + (ydiff ? ydiff : -ydiff);
+      });
+      self.shapesArr.push(new Polygon(tri.sides, tri.color));
+      self.isOnPolygon=false;
       self.clear();
-    } else {
-      self.triangleArr.push(new Triangle(self.imx, self.imy, self.fmx, self.fmy, getNewColor()));
     }
     event.preventDefault();
   });
   //double-click event listener
   canvas.addEventListener('dblclick', function(event){
-    self.triangleArr.splice(self.selectedPos, 1);
+    self.shapesArr.splice(self.selectedPos, 1);
     self.clear();
     event.preventDefault();
   });
   setInterval(function() {
     self.draw();
   }, 10);
-  document.getElementById("clear").addEventListener("click", cl);
-  function cl(){
-  self.triangleArr=[];
-  self.clear();
-  }
+
+  // click function
+  canvas.addEventListener('click', function(event) {
+    if(!self.allowToDraw) return false;
+    self.pointsArray.push({'x' : event.clientX, 'y' : event.clientY});
+    if(self.pointsArray.length === self.pointsLimit){
+      self.shapesArr.push(new Polygon(self.pointsArray, getNewColor()));
+      self.pointsArray = [];
+      self.allowToDraw = false;
+    }
+    console.log('click event called');
+  }, false);
+
+  document.getElementById("clear").addEventListener("click", function(){
+    self.shapesArr=[];
+    self.clear();
+  });
 };
 //function to draw triangle from array
 Canvas.prototype.draw = function(){
   // if(!this.isOnTriangle){
     let self = this;
-    this.triangleArr.forEach(function(triangle){
-      triangle.drawTriangles(self.ctx);
+    this.shapesArr.forEach(function(triangle){
+      triangle.drawPolygon(self.ctx);
     });
   // }
 };
 //function to clear canvas
 Canvas.prototype.clear = function(){
   this.ctx.clearRect(0, 0, this.width, this.height);
+};
+Canvas.prototype.initPolygon = function(length){
+  if(!length) return false;
+  console.log('initPolygon called');
+  var self = this;
+  self.allowToDraw = true;
+  this.pointsLimit = length;
+  this.ctx.fillText("Please Select points on the canvas", this.width/2-20, 50);
+  setTimeout(
+    function(){
+      self.ctx.fillText("", this.width/2-20, 50);
+    }
+  , 2000);
 };
--- shapes.js
+++ shapes.js
@@ -1,51 +1,11 @@
-function Triangle (ix, iy, fx, fy, rgb){
-  var ydiff = Math.abs(fx-iy);
-  var xc = Math.floor(Math.abs(ydiff/Math.tan(30))/2);
-  this.x1 = ix || 1;
-  this.y1 = iy || 1;
-  this.x2 = fx+xc || 2;
-  this.y2 = fy || 2;
-  this.x3 = fx-xc || 0;
-  this.y3 = fy || 1;
-  this.rgb = rgb || 'rgb(200, 200, 200)';
-};
-//function to draw triangle
-Triangle.prototype.drawTriangles = function(ctx){
-  ctx.beginPath();
-  ctx.moveTo(this.x1, this.y1);
-  ctx.lineTo(this.x2, this.y2);
-  ctx.lineTo(this.x3, this.y3);
-  ctx.lineTo(this.x1, this.y1);
-  ctx.lineWidth=2;
-  ctx.stroke();
-  ctx.fillStyle = this.rgb;
-  ctx.fill();
-};
-/*
-function to check position of click if it lies inside triangle
-it compares the area of the triangle with total area with
-formed by taking (x, y) as third point and other two point from triangle
-*/
-Triangle.prototype.checkPos = function(x, y){
-  let ar1 = getArea(this.x1, this.y1, this.x2, this.y2, x, y);
-  let ar2 = getArea(this.x1, this.y1, x, y, this.x3,this. y3);
-  let ar3 = getArea(x, y, this.x2, this.y2, this.x3, this.y3);
-  let area = getArea(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
-  return area === ar1+ar2+ar3;
-};
-//retur area of triangle
-function getArea(x1, y1, x2, y2, x3, y3){
-  return Math.abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
-};
-//______________________________________
-
 //class Polygon so there can be shapes with different nimber of sides
 function Polygon(sides, rgb){
   this.sides = sides;
-  this.sides = rgb || 'rgb(200, 200, 200)';
+  this.color = rgb || 'rgb(200, 200, 200)';
 }
 //Draw Polygon
-Polygon.prototype.draw = function(ctx){
+Polygon.prototype.drawPolygon = function(ctx){
+  let newPath = new Path2D();
   let startPosX, startPosY;
   ctx.beginPath();
   this.sides.forEach(function(s, i){
@@ -53,13 +13,21 @@ Polygon.prototype.draw = function(ctx){
       startPosX = s.x;
       startPosY = s.y;
       ctx.moveTo(s.x, s.y);
+      newPath.moveTo(s.x, s.y);
     } else {
       ctx.lineTo(s.x, s.y);
+      newPath.lineTo(s.x, s.y);
     }
   });
   ctx.lineTo(startPosX, startPosY);
   ctx.lineWidth=2;
   ctx.stroke();
-  ctx.fillStyle = this.rgb;
+  ctx.fillStyle = this.color;
   ctx.fill();
+
+  newPath.closePath();
+  this.polygonPath = newPath;
 }
+Polygon.prototype.checkPos = function(ctx, x, y){
+  return ctx.isPointInPath(this.polygonPath, x, y);
+};
